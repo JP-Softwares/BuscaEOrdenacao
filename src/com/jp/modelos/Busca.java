a434de78b5a9f7b46b6e9705cf171de47b454274
Geydson <geydson87@gmail.com>
2023-02-23 18:41:47 -0300

    Classes Busca e Ordenacao + java 8
    coloquei os códigos do computador da facul sobre busca e ordenação e mudei a plataforma do projeto para java 8.

--- src/com/jp/modelos/Busca.java
+++ src/com/jp/modelos/Busca.java
@@ -10,4 +10,23 @@ package com.jp.modelos;
  */
 public class Busca {
     
+    public static boolean sequencial(int A[], int x){
+        for(int i = 0; i < A.length; i++){
+            if(x == A[i]) return true;
+        }
+        
+        return false;
+    }
+    
+    public static boolean binaria(int A[], int x, int inicio, int fim){
+        if(inicio > fim) return false;
+        
+        int meio = (inicio + fim)/2;
+        
+        if(x == A[meio]) return true;
+        
+        if(x < A[meio]) return binaria(A, x, inicio, meio - 1);
+        
+        return binaria(A, x, meio + 1, fim);
+    }
 }
--- src/com/jp/modelos/Ordenacao.java
+++ src/com/jp/modelos/Ordenacao.java
@@ -10,4 +10,46 @@ package com.jp.modelos;
  */
 public class Ordenacao {
     
+    public static int[] selectionSort(int[] A){
+        
+        for(int i = 0; i <= A.length - 2; i++){
+            int min = i;
+            for(int j = i + 1; j <= A.length - 1; j++){
+                if(A[j] < A[min]) min = j;
+            }
+            
+            int t = A[min];
+            A[min] = A[i];
+            A[i] = t;
+        }
+        return A;
+    }
+    
+    public static int[] bubbleSort(int[] A){
+        for(int i = A.length - 1; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                if(A[j-1] > A[j]){
+                    int t = A[j-1];
+                    A[j-1] = A[j];
+                    A[j] = t;
+                }
+            }
+        }
+        return A;
+    }
+    
+    public static int[] insertionSort(int[] A){
+        for(int i = 1; i < A.length - 1; i++){
+            int v = A[i];
+            int j = i;
+            while (j > 0 && A[j-1] > v) {                
+                A[j] = A[j-1];
+                j = j-1;
+            }
+            A[j] = v;
+        }
+        
+        return A;
+    }
+    
 }
